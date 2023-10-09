94900b50bfad0dff2bc401f3d60afd16c5605771
slchangtw <shunlung.chang@gmail.com>
2023-10-09 21:40:24 +0800

    Remove shell script

--- ci_tests/.localstack/create_dynamodb_table.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-awslocal dynamodb create-table \
-   --table-name reverted_texts \
-   --attribute-definitions AttributeName=text,AttributeType=S  AttributeName=updated_at,AttributeType=S\
-   --key-schema AttributeName=text,KeyType=HASH AttributeName=updated_at,KeyType=RANGE \
-   --billing-mode PAY_PER_REQUEST \
-   --region us-east-1
