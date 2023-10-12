a0793edff9cc19a7653005d207fb702fa9017e33
slchangtw <shunlung.chang@gmail.com>
2023-10-12 15:56:53 +0800

    Add get_long_string.py

---
+++ ci_tests/src/get_long_string.py
@@ -0,0 +1,4 @@
+def get_long_string() -> str:
+    # This is just an example; in real code, you can
+    # use / or parentheses to break a long string.
+    return "1231231231231231231231231231231231231231231231231231231231231231231231231222222222"
